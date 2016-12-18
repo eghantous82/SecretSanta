7abdcfeb6c2d64b45e8597d1aa68014899906ae0
Eliass <eghantous82@gmail.com>
2016-12-17 22:20:30 -0500

    Initial implementation in ng Added styling to create initial look and feel for page.

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="content">
+    <message-input></message-input>
+    </div>
+    `
+})
+export class AppComponent {
+}
---
+++ app/message.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+@Component({
+  selector: 'message-input',
+  template: `
+    <div id="messageInfo">
+      <ul class="input-list style-1 clearfix">
+      <li>
+        <input type="text" [(ngModel)]="subject" placeholder="Subject" class="focus"/>
+      </li>
+      <li>
+        <textarea rows="20" [(ngModel)]="message" cols="80" placeholder="Message body" class="focus"></textarea>
+      </li>
+      <!--
+      Enter Message Subject: <input id="subject" type="text" size="80" required="" pattern="^(?!\s*$).+" onkeyup="validateEmail()"><br>
+      Enter Message Body:<br>
+      <textarea id="emailBody" required="" pattern="^(?!\s*$).+" onkeyup="validateEmail()" rows="20" cols="80"></textarea><br>
+      -->
+      </ul>
+    </div>
+    `
+})
+export class MessageComponent {
+  @Input()
+  subject = 'Subject From Variable';
+  message = 'Merry Christmas Folks!';
+}
